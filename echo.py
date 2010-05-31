3c5cae25c54a9c4299c8c7b47c2277c3b833b248
CongNT <ntcong.it@gmail.com>
2010-05-31 23:56:06 +0700

    ECHO complete

--- echo.py
+++ echo.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python
 
 import sys 
-print sys.argv
+import getopt
+
+VERSION = '1.0'
+display_return = True
+
+def print_usage():
+    print 'echo - display line of text'
+    print 'USAGE: echo [OPTION] [STRING]'
+    print '            -n  --no-newline        do not output trailing newline'
+    print '            -v  --version           output version information and exit'
+    print '            -h  --help              display this help and exit'
+    
+    
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],'nhv',['no-newline','help','version'])
+    except getopt.GetoptError, err:
+        print str(err)
+        print_usage()
+        return 
+    for arg,val in opts:
+        if arg in ('-h','--help'):
+            print_usage()
+            return
+        elif arg in ('-v','--version'):
+            print VERSION
+            return
+        elif arg in ('-n','--no-newline'):
+            display_return = False
+    
+    for val in args:
+        print val,
+    if display_return:
+        print 
+    
+if __name__=="__main__":
+    main()
+
+
